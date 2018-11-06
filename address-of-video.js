ab024ccea1430038611c497e8e53958944f7e9e9
Aleksandar Timanov <alexandartimanov@gmail.com>
2018-11-06 09:01:37 +0000

    trying if click to play works on phone

--- address-of-video.js
+++ address-of-video.js
@@ -1,5 +1,5 @@
 const videoSource = '/videos/360_VR Master Series _ Free Download _  Pebbly Beach Headland.mp4';
         
 var background = document.querySelector('#background');
-console.log(background);
+// console.log(background);
 background.setAttribute('src', videoSource);
