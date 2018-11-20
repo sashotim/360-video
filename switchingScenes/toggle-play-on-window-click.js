3de483225286b3ac09fc7546cb78d03c29e19ec0
Aleksandar Timanov <alexandartimanov@gmail.com>
2018-11-20 12:45:45 +0000

    trying touchend as the event

--- switchingScenes/toggle-play-on-window-click.js
+++ switchingScenes/toggle-play-on-window-click.js
@@ -1,22 +1,22 @@
 AFRAME.registerComponent('toggle-play-on-window-click', {
   init: function () {
     this.onClick = this.onClick.bind(this);
-    this.onTouch = this.onTouch.bind(this);
+    this.onTouchend = this.onTouchend.bind(this);
   },
   play: function () {
     window.addEventListener('click', this.onClick);
-    window.addEventListener('touch', this.onClick);
+    window.addEventListener('touchend', this.onClick);
   },
   pause: function () {
     window.removeEventListener('click', this.onClick);
-    window.removeEventListener('touch', this.onClick);
+    window.removeEventListener('touchend', this.onClick);
   },
   onClick: function (evt) {
     var video = this.el.components.material.material.map.image;
     if (!video) { return; }
     video.paused ? video.play() : video.pause();
   },
-  onTouch: function (evt) {
+  onTouchend: function (evt) {
     var video = this.el.components.material.material.map.image;
     if (!video) { return; }
     video.paused ? video.play() : video.pause();
