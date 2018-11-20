dade375bbf43641512454e2804a44c6410cbf996
Aleksandar Timanov <alexandartimanov@gmail.com>
2018-11-20 12:34:34 +0000

    trying to use listeners for touch event to fix play/pause problem

--- switchingScenes/toggle-play-on-window-click.js
+++ switchingScenes/toggle-play-on-window-click.js
@@ -1,14 +1,22 @@
 AFRAME.registerComponent('toggle-play-on-window-click', {
   init: function () {
     this.onClick = this.onClick.bind(this);
+    this.onTouch = this.onTouch.bind(this);
   },
   play: function () {
     window.addEventListener('click', this.onClick);
+    window.addEventListener('touch', this.onClick);
   },
   pause: function () {
     window.removeEventListener('click', this.onClick);
+    window.removeEventListener('touch', this.onClick);
   },
   onClick: function (evt) {
+    var video = this.el.components.material.material.map.image;
+    if (!video) { return; }
+    video.paused ? video.play() : video.pause();
+  },
+  onTouch: function (evt) {
     var video = this.el.components.material.material.map.image;
     if (!video) { return; }
     video.paused ? video.play() : video.pause();
